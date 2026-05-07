a492d9ea8185a7ae0e17af3660efcd8a56b70185
ESP Builder <noreply@example.com>
2026-05-07 13:26:45 +0100

    Settings UI: show AP credentials and hide WiFi signal row in AP mode

--- ESP32-S3_Square_Display/src/ui_Settings.h
+++ ESP32-S3_Square_Display/src/ui_Settings.h
@@ -9,6 +9,7 @@ extern lv_obj_t *ui_SettingsPanel;
 extern lv_obj_t *ui_BrightnessSlider;
 extern lv_obj_t *ui_BrightnessLabel;
 extern lv_obj_t *ui_IPLabel;
+extern lv_obj_t *ui_APCredsLabel;
 extern lv_obj_t *ui_RSSILabel;
 extern lv_obj_t *ui_RSSIBar;
 extern lv_obj_t *ui_BackButton;
